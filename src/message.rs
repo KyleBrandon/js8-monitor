d14291f059d380c54b624184a8152521f15b7c96
Kyle Brandon <kyle@brandfam.net>
2021-07-13 06:14:29 -0500

    sample json

--- src/message.rs
+++ src/message.rs
@@ -1,5 +1,32 @@
+/*
+use serde_json;
+use serde_json::Value;
+use serde_json::from_slice;
+use serde_json::json;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct JS8Message {
+{
+    "params": {
+        "NAME": "JS8Call", 
+        "UTC": 1624587300068, 
+        "VERSION": "2.2.0", 
+        "_ID": "125288100068"
+    }, 
+    "type": "PING", 
+    "value": ""
+}
 
 
+}
+
+let json: Value = from_slice(&read_buffer[0 .. bytes_read])?;
+
+
+*/
+ 
 pub struct Message {
     buffer: [u8; 1024],
 }
